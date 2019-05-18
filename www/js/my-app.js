68fed74022408d5d38497fc98083d9b7f7f6fa31
yashz05 <yashc1999@gmail.com>
2019-05-19 02:53:08 +0530

    Update my-app.js

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -24,7 +24,31 @@ var app = new Framework7({
     ]
     // ... other parameters
   });
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 var mainView = app.views.create('.view-main');
 
 // Handle Cordova Device Ready Event
@@ -55,3 +79,5 @@ function refreshIframe() {
     var ifr = document.getElementsByName('Right')[0];
     ifr.src = ifr.src;
 }
+
+
