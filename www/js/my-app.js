dedb859a40edf2529461616961b1de15901614ef
yashz05 <yashc1999@gmail.com>
2019-05-19 10:51:35 +0530

    Update my-app.js

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -81,5 +81,9 @@ function refreshIframe() {
     var ifr = document.getElementsByName('Right')[0];
     ifr.src = ifr.src;
 }
-
+function onDeviceReady() {
+    // Now safe to use the PhoneGap API
+    
+    StatusBar.styleLightContent();
+}
 
